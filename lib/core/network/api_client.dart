dc32df70535cc6582c484b2801e7937230a63eac
suojae <suojae.developer@gmail.com>
2025-04-24 22:23:20 +0900

    feat: api-client 추가

---
+++ lib/core/network/api_client.dart
@@ -0,0 +1,48 @@
+import 'package:dio/dio.dart';
+import 'package:rubbit_client/core/network/api_exception.dart';
+import 'package:rubbit_client/core/type/result.dart';
+
+class ApiClient {
+  final Dio _dio;
+
+  ApiClient(this._dio);
+
+  /// GET 요청
+  Future<Result<T, ApiException>> get<T>(
+      String path, {
+        Map<String, dynamic>? queryParameters,
+      }) async {
+    try {
+      final response = await _dio.get<T>(path, queryParameters: queryParameters);
+      return Result.success(response.data as T);
+    } on DioException catch (e) {
+      return Result.failure(_mapDioErrorToApiException(e));
+    } catch (e) {
+      return Result.failure(
+        ApiException(code: 'unknown', message: e.toString()),
+      );
+    }
+  }
+
+  /// POST 요청
+  Future<Result<T, ApiException>> post<T>(
+      String path, {
+        dynamic data,
+        Map<String, dynamic>? queryParameters,
+      }) async {
+    try {
+      final response = await _dio.post<T>(path, data: data, queryParameters: queryParameters);
+      return Result.success(response.data as T);
+    } on DioException catch (e) {
+      return Result.failure(_mapDioErrorToApiException(e));
+    } catch (e) {
+      return Result.failure(
+        ApiException(code: 'unknown', message: e.toString()),
+      );
+    }
+  }
+
+  ApiException _mapDioErrorToApiException(DioException e) {
+    return ApiException(code: 'dio_error', message: e.message ?? '알 수 없는 오류');
+  }
+}

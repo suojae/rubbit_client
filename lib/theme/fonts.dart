5a853ed7c1efa022a485189ba0265e3e9e02da4e
suojae <suojae.developer@gmail.com>
2025-04-24 19:47:50 +0900

    feat: 구글폰트 적용 소셜로그인 타이틀 추가

--- lib/theme/fonts.dart
+++ lib/theme/fonts.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppFonts {
   static const TextStyle _baseFont = TextStyle(fontFamily: "pretendard");
+
+  static TextStyle socialLoginTitle = GoogleFonts.margarine(fontSize: 24.0, fontWeight: FontWeight.w900, height: 1.45);
 
   static final TextStyle headingMd600 = _baseFont.copyWith(
       fontSize: 18.0, fontWeight: FontWeight.w600, height: 1.5);

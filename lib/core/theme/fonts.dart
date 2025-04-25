75584419188c626ea1cafa23b817624a45b3c6c7
suojae <suojae.developer@gmail.com>
2025-04-25 17:11:29 +0900

    fix: 미사용 폰트 제거

--- lib/core/theme/fonts.dart
+++ lib/core/theme/fonts.dart
@@ -2,88 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppFonts {
-  static const TextStyle _baseFont = TextStyle(fontFamily: "pretendard");
+  // static const TextStyle _baseFont = TextStyle(fontFamily: "pretendard");
 
-  static TextStyle socialLoginTitle = GoogleFonts.margarine(fontSize: 24.0, fontWeight: FontWeight.w900, height: 1.45);
-
-  static final TextStyle headingMd600 = _baseFont.copyWith(
-      fontSize: 18.0, fontWeight: FontWeight.w600, height: 1.5);
-
-  static final TextStyle headingSm500 = _baseFont.copyWith(
-      fontSize: 17.0, fontWeight: FontWeight.w500, height: 1.5);
-
-  static final TextStyle headingXs600 = _baseFont.copyWith(
-      fontSize: 16.0, fontWeight: FontWeight.w600, height: 1.5);
-
-  static final TextStyle titleXs700 = _baseFont.copyWith(
-      fontSize: 19.0, fontWeight: FontWeight.w700, height: 1.4);
-
-  static final TextStyle bodyLg700 = _baseFont.copyWith(
-      fontSize: 16.0, fontWeight: FontWeight.w700, height: 1.5);
-
-  static final TextStyle bodyMd500 = _baseFont.copyWith(
-      fontSize: 15.0, fontWeight: FontWeight.w500, height: 1.5);
-
-  static final TextStyle bodySm500 = _baseFont.copyWith(
-      fontSize: 14.0, fontWeight: FontWeight.w500, height: 1.5);
-
-  static final TextStyle textMd500 = _baseFont.copyWith(
-      fontSize: 15.0, fontWeight: FontWeight.w500, height: 1.45);
-
-  static final TextStyle textMd600 = _baseFont.copyWith(
-      fontSize: 15.0, fontWeight: FontWeight.w600, height: 1.45);
-
-  static final TextStyle textMd400 = _baseFont.copyWith(
-      fontSize: 15.0, fontWeight: FontWeight.w400, height: 1.45);
-
-  static final TextStyle textSm500 = _baseFont.copyWith(
-      fontSize: 14.0, fontWeight: FontWeight.w500, height: 1.45);
-
-  static final TextStyle textXs600 = _baseFont.copyWith(
-      fontSize: 13.0, fontWeight: FontWeight.w600, height: 1.45);
-
-  static final TextStyle textXs500 = _baseFont.copyWith(
-      fontSize: 13.0, fontWeight: FontWeight.w500, height: 1.45);
-
-  static final TextStyle textXs400 = _baseFont.copyWith(
-      fontSize: 13.0, fontWeight: FontWeight.w400, height: 1.35);
-
-  static final TextStyle inputTextfiledLimitNumber = _baseFont.copyWith(
-      fontSize: 13.0, fontWeight: FontWeight.w500, height: 1.35);
-
-  static final TextStyle captionSm500 = _baseFont.copyWith(
-      fontSize: 12.0, fontWeight: FontWeight.w500, height: 1.4);
-
-  static final TextStyle labelMd600 = _baseFont.copyWith(
-      fontSize: 14.0, fontWeight: FontWeight.w600, height: 1.4);
-
-  static final TextStyle tab = _baseFont.copyWith(
-      fontSize: 10.0, fontWeight: FontWeight.w500, height: 1.35);
-
-  static final TextStyle info = _baseFont.copyWith(
-      fontSize: 12.0, fontWeight: FontWeight.w400, height: 1.2);
-
-  static final TextStyle placeMainTitle = _baseFont.copyWith(
-      fontSize: 16.0, fontWeight: FontWeight.w400, height: 1.5);
-
-  static final TextStyle placeSubTitle = _baseFont.copyWith(
-      fontSize: 14.0, fontWeight: FontWeight.w400, height: 1.5);
-
-  static final TextStyle placeDistance = _baseFont.copyWith(
-      fontSize: 13.0, fontWeight: FontWeight.w400, height: 1.4);
-
-  static final TextStyle signinHeader = _baseFont.copyWith(
-      fontSize: 24.0, fontWeight: FontWeight.w600, height: 1.45);
-
-  static final TextStyle signupHeader = _baseFont.copyWith(
-      fontSize: 22.0, fontWeight: FontWeight.w600, height: 1.45);
-
-  static final TextStyle myPageNickName = _baseFont.copyWith(
-      fontSize: 15.0, fontWeight: FontWeight.w600, height: 1.5);
-
-  static final TextStyle settingSectionHeader = _baseFont.copyWith(
-      fontSize: 14.0, fontWeight: FontWeight.w600, height: 1.5);
-
-  static final TextStyle lociTextFieldTitle = _baseFont.copyWith(
-      fontSize: 13.0, fontWeight: FontWeight.w500, height: 1.35);
+  static TextStyle socialLoginTitle = GoogleFonts.margarine(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w900,
+    height: 1.45,
+  );
 }
